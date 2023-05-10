bdd4975f52af60bdaee2a9bfed820706ad11f8de
Bonni99 <127943467+Bonni99@users.noreply.github.com>
2023-05-10 11:43:53 +0200

    primo commit vue

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Gallery from './components/Gallery.vue'
+
+</script>
+
+<template>
+  <header>
+    
+    <div class="wrapper">
+      
+        
+      
+    </div>
+  </header>
+
+  <main>
+    <div class="gallery">
+    <Gallery msg="Streetwear" />
+      <Gallery msg="Music" />
+        <Gallery msg="Festival" />
+  </div>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+.gallery{
+  display:flex;
+  align-items: flex-start;
+  
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/Gallery.vue
@@ -0,0 +1,70 @@
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    imageUrl: String,  }
+})
+</script>
+
+
+
+
+<template>
+
+<article class="article">
+  <img src="src/image/vale-rjVM5h_e5yA-unsplash.jpg" width="200" alt="">
+  <h2>Gallery 
+     <p>Questo è un paragrafo per questo articolo</p>
+  </h2>
+</article>
+</template>
+
+<style scoped>
+
+
+
+
+img{
+  position: absolute;
+  display: block;
+  z-index: -10
+
+  
+}
+
+p{
+margin-right: 15px;
+}
+
+h2{
+  margin: 15px;
+  padding: 9px;
+  display:block;
+  width: 30%;
+  display: block;
+  
+  font-weight: 700;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-size: medium;
+
+
+  color: #fff;
+
+}
+
+.article :hover{
+  color: linear-gradient(0deg, blue, white);
+
+}
+
+
+h1{
+  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+  font-weight: 800;
+  }
+
+
+
+</style>
+
